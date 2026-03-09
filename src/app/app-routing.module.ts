e638706c0565d6751982152420f9f72d336d8142
markostojicic03 <mstojicic12923rn@raf.rs>
2026-03-09 15:10:52 +0100

    Setting Angular Project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) }, { path: 'user', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
